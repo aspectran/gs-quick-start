c40234a77103291adb771ee007f8d538fc396ad9
Juho Jeong <aspectran@gmail.com>
2017-03-21 17:25:27 +0900

    Convert tabs to 4 spaces

--- src/main/java/sample/SimplestAction.java
+++ src/main/java/sample/SimplestAction.java
@@ -5,15 +5,15 @@ import com.aspectran.core.util.logging.Log;
 import com.aspectran.core.util.logging.LogFactory;
 
 public class SimplestAction {
-	
-	private final Log log = LogFactory.getLog(SimplestAction.class);
+    
+    private final Log log = LogFactory.getLog(SimplestAction.class);
 
-	public String helloWorld(Translet translet) {
-		String msg = translet.getMessage("hello", new Object[] { "Aspectran" });
-		
-		log.info(translet.getMessage("first.action.is", new Object[] { msg }));
+    public String helloWorld(Translet translet) {
+        String msg = translet.getMessage("hello", new Object[] { "Aspectran" });
+        
+        log.info(translet.getMessage("first.action.is", new Object[] { msg }));
 
-		return msg;
-	}
+        return msg;
+    }
 
 }
--- src/main/java/sample/SimplestAdvice.java
+++ src/main/java/sample/SimplestAdvice.java
@@ -7,27 +7,27 @@ import com.aspectran.core.util.logging.Log;
 import com.aspectran.core.util.logging.LogFactory;
 
 public class SimplestAdvice {
-	
-	private final Log log = LogFactory.getLog(SimplestAdvice.class);
+    
+    private final Log log = LogFactory.getLog(SimplestAdvice.class);
 
-	public String welcome(Translet translet) {
-		HttpServletRequest req = translet.getRequestAdaptee();
-		String ip = req.getRemoteAddr();
-		String msg = "Welcome to Aspectran! (" + ip + ")";
-		
-		log.info(msg);
-		
-		return msg;
-	}
-	
-	public String goodbye(Translet translet) {
-		log.info("activityDataMap " + translet.getActivityDataMap(true));
+    public String welcome(Translet translet) {
+        HttpServletRequest req = translet.getRequestAdaptee();
+        String ip = req.getRemoteAddr();
+        String msg = "Welcome to Aspectran! (" + ip + ")";
+        
+        log.info(msg);
+        
+        return msg;
+    }
+    
+    public String goodbye(Translet translet) {
+        log.info("activityDataMap " + translet.getActivityDataMap(true));
 
-		String msg = "Goodbye!";
-		
-		log.info(msg);
-		
-		return msg;
-	}
-	
+        String msg = "Goodbye!";
+        
+        log.info(msg);
+        
+        return msg;
+    }
+    
 }
