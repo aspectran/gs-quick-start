22a9fcb4ced27f8fe2b2542c4b5df80c06bf155d
Juho Jeong <aspectran@gmail.com>
2021-01-07 22:13:17 +0900

    Update

--- src/main/java/sample/SampleActivity.java
+++ src/main/java/sample/SampleActivity.java
@@ -4,9 +4,9 @@ import com.aspectran.core.activity.Translet;
 import com.aspectran.core.util.logging.Logger;
 import com.aspectran.core.util.logging.LoggerFactory;
 
-public class SampleAction {
+public class SampleActivity {
     
-    private final Logger logger = LoggerFactory.getLogger(SampleAction.class);
+    private final Logger logger = LoggerFactory.getLogger(SampleActivity.class);
 
     public String helloWorld(Translet translet) {
         String msg = translet.getMessage("hello", new Object[] { "Aspectran" });
